3b9682b5900cab8abfa93254a8b59efdb1852074
ibai71 <96577798+ibai71@users.noreply.github.com>
2024-11-23 15:40:42 +0100

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -2,7 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
+function iniciarBD() {
+    const solicitud = indexedDB.open("vitomaite04", 1);
+    console.log("Intentando abrir la base de datos...");
+    
 
+    solicitud.onsuccess = function(evento) {
+        bd = evento.target.result;
+        console.log("Base de datos abierta exitosamente", bd);
+
+        // Llamar a cargarResultadosBusqueda solo cuando la base de datos esté disponible
+        cargarResultadosBusqueda();
+    };
+
+    solicitud.onerror = function(evento) {
+        console.error("Error al abrir la base de datos:", evento.target.error);
+    };
+
+    solicitud.onupgradeneeded = function(evento) {
+        bd = evento.target.result;
+        console.log("Actualizando la base de datos (onupgradeneeded)");
+        // Configura los object stores aquí si es necesario
+    };
+}
 
 function handleSearchClick() {
     // Redireccionar a la página que deseas (por ejemplo, resultados.html)
